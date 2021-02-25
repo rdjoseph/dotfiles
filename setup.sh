1b8cf36bfccd8f55c597bca23a8d3ae92f5da84b
rdjoseph <rdjoseph@uvm.edu>
2021-02-25 17:45:54 -0500

    Fix setup script for exit message

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.vim ~/.vim 
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
+cp see_you_space_cowboy.sh ~/see_you_space_cowboy.sh # A symbolic link won't work because there's too many levels of symbolic linking involved
