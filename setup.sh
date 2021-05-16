a0b5b11ed0edd4f0bf92df196ffbbe4a3a67711c
rdjoseph <rdjoseph@uvm.edu>
2021-05-16 19:55:46 -0400

    Add .emacs ln to set up script

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.vim ~/.vim 
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.emacs ~/.emacs
 cp see_you_space_cowboy.sh ~/see_you_space_cowboy.sh # A symbolic link won't work because there's too many levels of symbolic linking involved
